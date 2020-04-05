a2556d7b666bea6db5c43e142b5f2bfbbb7c9053
Fikri <f.n.wijaya1@gmail.com>
2020-04-06 04:26:35 +0700

    latest update

--- services/DbService.go
+++ services/DbService.go
@@ -4,6 +4,8 @@ import (
 	"ThesisProject/models"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"os"
 )
 
 var Db *gorm.DB
@@ -11,7 +13,15 @@ var Db *gorm.DB
 func init() {
 	//open a db connection
 	var err error
-	Db, err = gorm.Open("mysql", "root:password@/golang?charset=utf8&parseTime=True&loc=Local")
+	port := os.Getenv("PORT")
+	if port != "" {
+		Db, err = gorm.Open("postgres", "host=ec2-52-71-85-210.compute-1.amazonaws.com"+
+			" port=5432 user=zswecpzsyyjkqz dbname=d1fmiuhjc25u5f"+
+			" password=9ad7aeeeacc2ca1ff64f9cb29c7ae37a9127a905f9c3267aeafe90207405886a")
+	} else {
+		Db, err = gorm.Open("mysql", "root:password@/golang?charset=utf8&parseTime=True&loc=Local")
+	}
+
 	if err != nil {
 		panic("failed to connect database")
 	}
